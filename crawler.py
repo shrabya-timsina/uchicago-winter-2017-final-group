d81fe02c36eaa3d9670e7778b1bd3fac2bc3b365
Shrabya Timsina <shrabya@cs.uchicago.edu>
2017-02-06 19:29:08 -0600

    some work done

--- crawler.py
+++ crawler.py
@@ -4,7 +4,7 @@ import requests
 import urllib.parse
 
 
-def crawler(starting_url, limiting_domain, course_map_filename):
+def crawler(starting_url):
     '''
     Crawl the college catalog and generate a dictionary with course 
     identifiers mapped to words as key-value pairs
@@ -17,63 +17,34 @@ def crawler(starting_url, limiting_domain, course_map_filename):
         index_dictionary: dictionary, with course identifiers mapped to
                         words as key-value pairs
     '''
-
-    course_code_identifier_map = open_json_key(course_map_filename)
-    urls_to_crawl = queue.Queue()
-
-    urls_crawled = set()
-
-    urls_processed = set() 
-
-    urls_to_crawl.put(starting_url)
-    urls_crawled.add(starting_url)
-    crawl_count = 0
-    index_dictionary = {}
     
-    while (not urls_to_crawl.empty()) and (crawl_count < 1000):
-        
-        next_to_crawl = urls_to_crawl.get()
-        request = get_request(next_to_crawl)
-        
-        if request is not None:
-            real_url = get_request_url(request)
+    user_beers_url = starting_url + "/beers"
+    request = get_request(user_beers_url)
+    soup = convert_to_soup(request)
+    tag_list = soup.find_all("p", "name")
+    beers_urls_list = []
+    for tag in tag_list:
+        beer_url = tag.contents[0].get('href')
+        absolute_beer_url = convert_if_relative_url(user_beers_url, beer_url)
+        beers_urls_list.append(absolute_beer_url)
 
-            if real_url is not None:
-                
-                if real_url not in urls_crawled:
-                    urls_crawled.add(real_url)
-                    
-                if real_url not in urls_processed:
-                    urls_processed.add(real_url)
-                    soup = convert_to_soup(request)
+    users_to_crawl = []
+    for beer_url in beers_urls_list:
+        request = get_request(beer_url)
+        if request == None:
+            continue
+        else:
+            soup = convert_to_soup(request)
+            tag_list = soup.find_all("div","avatar-holder")[:-2]
+            for user in tag_list:
+                user_url = user.find('a').get('href')
+                absolute_user_url = convert_if_relative_url(beer_url, user_url)
+                if (absolute_user_url != starting_url) and (absolute_user_url not in users_to_crawl):
+                    users_to_crawl.append(absolute_user_url)
 
-                    if soup is not None:
+    return users_to_crawl
 
-                        #update the dictionary
-                        index_dictionary = build_dict(soup, index_dictionary, 
-                                                   course_code_identifier_map)
 
-                        #check for potential links in current page 
-                        #and update the queue
-                        list_of_urls_in_page = soup.find_all('a', href=True)
-
-                        for link in list_of_urls_in_page:
-                            url = extract_url(link, real_url)
-
-                            if url is not None:
-
-                                if (url not in urls_crawled):
-
-                                    if is_url_ok_to_follow(url, 
-                                                           limiting_domain):
-
-                                        urls_to_crawl.put(url)
-                                        urls_crawled.add(url)
- 
-
-        crawl_count += 1 
-
-    return index_dictionary 
 
 ########from util#############################################################################
 
@@ -105,7 +76,7 @@ def get_request(url):
 
     return r
 
-    def read_request(request):
+def read_request(request):
     '''
     Return data from request object.  Returns result or "" if the read
     fails..
@@ -181,62 +152,10 @@ def convert_if_relative_url(current_url, new_url):
         return urllib.parse.urljoin(current_url, new_url)
 
 
-def is_url_ok_to_follow(url, limiting_domain):
-    '''
-    Inputs:
-        url: absolute URL
-        limiting domain: domain name
-
-    Outputs: 
-        Returns True if the protocol for the URL is HTTP, the domain
-        is in the limiting domain, and the path is either a directory
-        or a file that has no extension or ends in .html. URLs
-        that include an "@" are not OK to follow.
-
-    Examples:
-        is_url_ok_to_follow("http://cs.uchicago.edu/pa/pa1", "cs.uchicago.edu") yields
-            True
-
-        is_url_ok_to_follow("http://cs.cornell.edu/pa/pa1", "cs.uchicago.edu") yields
-            False
-    '''
-
-    if "mailto:" in url:
-        return False
-
-    if "@" in url:
-        return False
-
-    if url[:LEN_ARCHIVES] == ARCHIVES:
-        return False
-
-    parsed_url =  urllib.parse.urlparse(url)
-    if parsed_url.scheme != "http" and parsed_url.scheme != "https":
-        return False
-
-    if parsed_url.netloc == "":
-        return False
-
-    if parsed_url.fragment != "":
-        return False
-
-    if parsed_url.query != "":
-        return False
-
-
-    loc = parsed_url.netloc
-    ld = len(limiting_domain)
-    trunc_loc = loc[-(ld+1):]
-    if not (limiting_domain == loc or (trunc_loc == "." + limiting_domain)):
-        return False
-
-    # does it have the right extension
-    (filename, ext) = os.path.splitext(parsed_url.path)
-    return (ext == "" or ext == ".html")
-
  #####################################################################################
 
-def convert_to_soup(url):
+
+def convert_to_soup(request):
     '''
     Convert a given request object to soup
 
@@ -247,25 +166,9 @@ def convert_to_soup(url):
         returns None otherwise    
     '''
 
-    pm = urllib3.PoolManager()
-
-    html = pm.urlopen(url=myurl, method="GET").data
-    soup = bs4.BeautifulSoup(html, "lxml")
-
-def extract_url(tag, absolute_url):
-    '''
-    Given a tag with an attribute href=True, will return an absolute url
-    of the tag
-
-    Inputs:
-        tag: a tag with an href attribute
-        absolute_url: string, the full url address of the tag
-
-    Outputs:
-        url: string, the link provided in the tag
-    '''
-    
-    url = tag.get('href')
-    url = remove_fragment(url)
-    url = convert_if_relative_url(absolute_url, url)
-    return url
+    html = read_request(request)
+    if html is not None:
+        soup = bs4.BeautifulSoup(html, 'lxml')
+        return soup
+    else:
+        return None
