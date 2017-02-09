11f69e48a2abe5bdffcb1f18055b073bc7b26a95
Sandeep Malladi <sandeepm@cs.uchicago.edu>
2017-02-09 13:46:08 -0600

    functions for data collection and object creation

---
+++ data_collect.py
@@ -0,0 +1,30 @@
+# cs122 project 
+#
+# Sandeep Malladi
+
+import sys
+import util
+import crawler
+import beer
+import user
+
+
+def get_user_from_url(url):
+    '''
+    given a url string that links to a user page, 
+    collect relevant data, build & return user object
+    '''
+    
+    
+    
+    
+def get_beer_from_url(url):
+    '''
+    given a url string that links to a beer page,
+    collect relevant data, build & return beer object
+    '''
+    
+    
+    
+    
+    
