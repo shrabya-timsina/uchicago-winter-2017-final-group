063ba40f521ccd2bf9e48efa08c2fc1307043f67
Sandeep Malladi <sandeepm@cs.uchicago.edu>
2017-02-09 14:23:27 -0600

    updated data_collect

--- data_collect.py
+++ data_collect.py
@@ -14,6 +14,8 @@ def get_user_from_url(url):
     given a url string that links to a user page, 
     collect relevant data, build & return user object
     '''
+    request = crawler.get_request(url)
+    soup = crawler.convert_to_soup(request)
     
     
     
@@ -23,7 +25,9 @@ def get_beer_from_url(url):
     given a url string that links to a beer page,
     collect relevant data, build & return beer object
     '''
-    
+    request = crawler.get_request(url)
+    soup = crawler.convert_to_soup(request)
+
     
     
     
