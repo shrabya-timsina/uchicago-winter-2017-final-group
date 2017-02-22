0caf30358abab18fef5818daabe7137e662d1a1c
Shrabya Timsina <shrabya@cs.uchicago.edu>
2017-02-21 19:31:10 -0600

    selenium running

--- selenium_automation.py
+++ selenium_automation.py
@@ -3,53 +3,68 @@
 # password: csproject
 #
 """
+if anyone wants torun selenium -use Vm and on terminal enter the following two 
+lines update your VM, which will update firefox too:
 sudo apt-update
 sudo apt-upgrade
+___________
+turning sth into an executable - for future reference
 chmod +x firefox
 chmod +x firefox-bin
 ./firefox
-
 """
 
-#on terminal: export PATH=$PATH:/home/shrabya/team-cs122-project
+#run the following on terminal before running selenium - every time
+#we need to make a script later which runs both the following and our entire project
+#on terminal: export PATH=$PATH:/home/student/team-cs122-project
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-
+from time import sleep
 user_url = 'https://untappd.com/user/Em11'
 
 
 def go():
-    #binary = FirefoxBinary('/home/student/team-cs122-project/firefox')
-    #bin = FirefoxBinary('/usr/bin')
     browser = webdriver.Firefox()
+    browser.get(user_url)
+    browser.find_element_by_link_text('Sign In').click()
+    sleep(1)
 
+    username = browser.find_element_by_id("username")
+    password = browser.find_element_by_id("password")
+    username.send_keys("cs122")
+    password.send_keys("csproject")
+    password.submit()
 
 
 def get_full_page_from_user_url(user_url):
     '''
     automate clicking through "show more" button on user profile feed
     '''
-    binary = FirefoxBinary('/home/student/team-cs122-project/firefox')
-    bin = FirefoxBinary('/usr/bin')
-    browser = webdriver.Firefox(firefox_binary=binary)
+    browser = webdriver.Firefox()
     browser.get(user_url)
     # login
-    browser.execute_script("document.getElementsByClassName('sign_in track-click')[0].click()")
+    browser.find_element_by_link_text('Sign In').click()
     # next 5 lines from http://stackoverflow.com/questions/21186327/fill-username-and-password-using-selenium-in-python
+    sleep(1)
     username = browser.find_element_by_id("username")
     password = browser.find_element_by_id("password")
     username.send_keys("cs122")
     password.send_keys("csproject")
-    browser.find_element_by_name("submit").click()
+    password.submit()
+    sleep(1)
+    
     # after login, browser redirects to user_url page -- need to see if selenium prevents that somehow
-
+    #update: works fine
     # click on show more until the full feed is visible
+    
     x = 0
     # temporary hard code loop until dynamic loop in place
     # even when show more button no longer appears in browser, the button still exists in html (see page source)
     # so no problem with running the loop too many times
     while x < 15:
+        print("step",x)
+        sleep(1)
         # code from http://stackoverflow.com/questions/36987006/how-to-click-a-javascript-button-with-selenium
         browser.execute_script("document.getElementsByClassName('yellow button more_checkins more_checkins_logged track-click')[0].click()")
         x += 1
