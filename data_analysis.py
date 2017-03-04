83e59fca7188711a88b8a91a4b8b028e4846be62
Luis Buenaventura Valcke <lbvalcke@cs.uchicago.edu>
2017-03-03 21:48:37 -0600

    finished style similarities

--- data_analysis.py
+++ data_analysis.py
@@ -13,11 +13,11 @@ import data_process as dProc
 # SQLite3
 DB_FILENAME = 'teamcs122db.db'
 
-def vectorize_sql_profile(user_url):
+def vectorize_beers(user_url):
 
     connect = sql.connect(DB_FILENAME)
     user_vectors = pd.read_sql('SELECT DISTINCT username from beer_user_info', connect)
-    usernames = user_vectors
+    usernames = user_vectors.copy()
     for index, row in usernames.iterrows():
         username = row['username']
         beers = pd.read_sql('SELECT beer_id, rating, count from beer_user_info where username like ' + '\'' + str(username) + '\'', connect)
@@ -35,7 +35,7 @@ def vectorize_sql_profile(user_url):
     user_vectors.set_index('username', drop=True, inplace=True)
     test_dict = crawler.profile_scraper(user_url)
     username = test_dict['username']
-    test_vector = pd.DataFrame(columns=list(user_vectors.columns.values), index=[0])
+    test_vector = pd.DataFrame(columns=list(user_vectors.columns.values), index=[username])
     test_vector.fillna(0, inplace=True)
     total_count = 0
     
@@ -51,25 +51,58 @@ def vectorize_sql_profile(user_url):
             test_vector[beer_id] = float(count * rating)
     
     test_vector = test_vector.divide(total_count, axis='index')
+    distance_df = calc_cos_simil(test_vector, user_vectors)
+
+    distance_df = vectorize_styles(test_dict, usernames)
+    connect.close()
+
+def calc_cos_simil(test_vector, user_vectors):
+    i = 0
     n = len(user_vectors)
     distance_matrix = np.zeros((n, 1))
-    
     test_matrix = test_vector.as_matrix()
     user_matrix = user_vectors.as_matrix()
-    i = 0
+    
     for row in user_matrix:
         distance_matrix[i,0] = cosine_similarity(test_matrix, row)
         i += 1
     distance_df = pd.DataFrame(distance_matrix, index=user_vectors.index.tolist(), columns=['score']).sort(columns='score', ascending=False)
-    print(distance_df)
+    return distance_df
+
+
+def vectorize_styles(user_dict, usernames):
+    connect = sql.connect(DB_FILENAME)
+    user_vectors = usernames
+    for index, row in usernames.iterrows():
+        username = row['username']
+        styles = pd.read_sql('SELECT username, style, count FROM style_counts where username like ' + '\'' + str(username) + '\'', connect)
+        total_count = styles['count'].values.sum()
+        for sub_index, sub_row in styles.iterrows():
+            style = sub_row['style']
+            rel_freq = float(sub_row['count']) / total_count
+            if style not in list(user_vectors.columns.values):
+                user_vectors[style] = 0
+                user_vectors.ix[index, style] = float(rel_freq)
+            else:
+                user_vectors.ix[index, style] = float(rel_freq)
+
+    user_vectors.set_index('username', drop=True, inplace=True)
+    username = user_dict['username']
+    test_vector = pd.DataFrame(columns=list(user_vectors.columns.values), index=[username])
+    test_vector.fillna(0, inplace=True)
+    total_count = 0
+    
+    for style in user_dict['styles'].keys():
+        count = int(user_dict['styles'][style])
+        total_count += count
+        if style not in list(user_vectors.columns.values):
+            user_vectors[style] = 0
+            test_vector[style] = float(count) 
+        else: 
+            test_vector[style] = float(count)
+
+    test_vector = test_vector.divide(total_count, axis='index')
+    distance_df = calc_cos_simil(test_vector, user_vectors)
+    print(distance_df.head())
 
     connect.close()
-
-
-
-
-
-
-
-
-
